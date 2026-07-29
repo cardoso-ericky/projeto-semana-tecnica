41077f7404dc6be27652a3a39bc3824d35eb18a1
I see you user <bryansamarone@gmail.com>
2026-07-29 11:46:38 -0300

    Implement book management and loan queries
    Added functions to manage books and query loans.

--- back-end/banco.js
+++ back-end/banco.js
@@ -7,10 +7,41 @@ const connection = mysql.createConnection({
   database: 'biblioteca'
 });
 
-connection.query(
-  'SELECT * FROM usuarios',
-  function(err, results, fields) {
-    if (err) throw err;
-    console.log(results);
-  }
-);
+// connection.query(
+//   'SELECT * FROM livro',
+//   function(err, results, fields) {
+//     if (err) throw err;
+//     console.log(results);
+//   }
+// );
+
+function teste(){
+  connection.query(`SELECT * FROM Empréstimo`, 
+    function(err, results, fields) {
+        if (err) throw err;
+        console.log(results);
+      });
+}
+
+// teste();
+
+function adicionarLivro(idLivro, titulo, edicao, editora, unidades, genero, ano){
+    const comando = `INSERT INTO livro (idLivro, titulo, edicao, editora, unidades, genero, ano) VALUES (?, ?, ?, ?, ?, ?, ?);`;
+    const valores = [idLivro, titulo, edicao, editora, unidades, genero, ano];    
+    connection.query(comando, valores, function(err, results, fields) {
+        if (err) throw err;
+        console.log(results);
+      });
+}
+
+function consultarLivros(){
+  connection.query(`SELECT * FROM livro;`, 
+    function(err, results, fields) {
+        if (err) throw err;
+        console.log(results);
+      });
+}
+
+// adicionarLivro(4, 'Memórias Postumas de Brás Cubas', '23', 'Marcelo', 12, 'Literatura Brasileira', 2003);
+// consultarLivros();
+
